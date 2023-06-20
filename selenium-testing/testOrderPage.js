19a2e7f8d90096b72940482fa2993bdaeb8dddc2
Ema <ema.jamakovic@stu.ibu.edu.ba>
2023-06-21 00:21:56 +0200

    orderPage and OrderTicketNumber test

---
+++ selenium-testing/testOrderPage.js
@@ -0,0 +1,31 @@
+const { Builder, By, until} = require('selenium-webdriver');
+
+async function runTest() {
+  let driver = await new Builder().forBrowser('chrome').build(); 
+
+  try {
+    await driver.manage().window().maximize();
+    await driver.get('http://localhost:3001/'); 
+
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/div/div/div[1]/div/div/a/div/button')), 5000).click();
+
+
+    await driver.findElement(By.xpath('//*[@id="root"]/div/div/div/a/div/button')).click();
+
+
+    let element = await driver.wait(until.elementLocated(By.className('o-cast')), 10000);
+    let text = await element.getText();
+
+    if (text === 'Camille Rows, Sofiane Zermani, Cesar Domboy') {
+      console.log('Text matches.Test passed!');
+    } else {
+      console.log(`Test failed.`);
+    }
+  } catch (error) {
+    console.error('Test failed:', error);
+  } finally {
+    await driver.quit();
+  }
+}
+
+runTest();
--- selenium-testing/testOrderTicketNumber.js
+++ selenium-testing/testOrderTicketNumber.js
@@ -7,16 +7,17 @@ async function runTest() {
     await driver.manage().window().maximize();
     await driver.get('http://localhost:3001/'); // Replace with your app's URL
 
-    await driver.wait(until.elementLocated(By.css('a[href="/movies/647f00fef4d7ecd1902de377"]')), 5000).click();
+    await driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/div/div/div[1]/div/div/a/div/button')), 5000).click();
 
 
     await driver.findElement(By.xpath('//*[@id="root"]/div/div/div/a/div/button')).click();
+    await driver.findElement(By.xpath('/html/body/div/div/div/div[1]/div[1]/div[2]/div[4]/button[2]')).click();
 
-
-    let element = await driver.wait(until.elementLocated(By.className('o-cast')), 10000);
+   
+    let element = await driver.wait(until.elementLocated(By.css('#root > div > div > div.order-movie > div.cont1 > div.details > div.ticket-number > div')), 5000);
     let text = await element.getText();
 
-    if (text === 'Camille Rows, Sofiane Zermani, Cesar Domboy') {
+    if (text === '2') {
       console.log('Text matches.Test passed!');
     } else {
       console.log(`Text is "${text}". Test failed.`);
